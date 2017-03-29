292b8e993f236b1b422e5dd31464c78c6f4fda05
Alberto Cole <acpii2005@gmail.com>
2017-03-29 15:06:50 -0600

    more updates, babel and webpack config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
-let test = 'test';
+const torrolo = {torrolo : 'torrolo'};
+export default torrolo;
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+/*global __dirname, require, module*/
+
+const webpack = require('webpack');
+const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+const path = require('path');
+
+let plugins = [], outputFile;
+
+plugins.push(new UglifyJsPlugin({ minimize: true }));
+outputFile = 'library.min.js';
+
+const config = {
+  entry: __dirname + '/src/index.js',
+  devtool: 'source-map',
+  output: {
+    path: __dirname + '/lib',
+    filename: outputFile,
+    library: 'library',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/
+      },
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: "eslint-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve('./src')],
+    extensions: ['.json', '.js']
+  },
+  plugins: plugins
+};
+
+module.exports = config;
