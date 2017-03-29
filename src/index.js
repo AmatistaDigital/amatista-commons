7d008ffed55067e57195096649df9afcd6f43621
Alberto Cole <acpii2005@gmail.com>
2017-03-29 16:36:39 -0600

    porting aws functions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import joi from 'joi';
+import AWS from 'aws-sdk';
 
 export const joiValidate = (object, schema) => new Promise((resolve, reject) => {
   joi.validate(object, schema, (err, value) => {
@@ -7,5 +8,27 @@ export const joiValidate = (object, schema) => new Promise((resolve, reject) => 
       return;
     }
     resolve(value);
+  });
+});
+
+export const dynamoQuery = (parameters) => new Promise((resolve, reject) => {
+  const dynamo = new AWS.DynamoDB.DocumentClient();
+  dynamo.query(parameters, (err, result) => {
+    if (err) {
+      reject(err);
+      return;
+    }
+    resolve(result);
+  });
+});
+
+export const dynamoPut = (parameters) => new Promise((resolve, reject) => {
+  const dynamo = new AWS.DynamoDB.DocumentClient();
+  dynamo.put(parameters, (error, result) => {
+    if (error) {
+      reject(error);
+      return;
+    }
+    resolve(result);
   });
 });
